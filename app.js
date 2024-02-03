8d3e2d3281459150a03f6ad587a515a23d739659
Muhammad Tier Sinyo C.U.S <118105588+Stalker-moment@users.noreply.github.com>
2024-02-03 09:57:18 +0700

    update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 const port = 2030;
+ 
+// Menyajikan file statis public
+app.use(express.static('./public'));
 
 // Menyajikan file index.html saat permintaan root dilakukan
 app.get('/', (req, res) => {
