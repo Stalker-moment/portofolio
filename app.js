22d806afc76cb24cf45b2f8155ed4f495e4c3b7a
Muhammad Tier Sinyo C.U.S <118105588+Stalker-moment@users.noreply.github.com>
2024-02-03 10:10:22 +0700

    fix bug

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 2030;
  
 // Menyajikan file statis public
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname)));
 
 // Menyajikan file index.html saat permintaan root dilakukan
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
