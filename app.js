3023d21fbfc0c86454c16f4c88bf216374f14d9e
Muhammad Tier Sinyo C.U.S <118105588+Stalker-moment@users.noreply.github.com>
2024-02-03 09:10:03 +0700

    up besar2an

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 const port = 2030;
-
-// Menggunakan middleware express.static untuk menyajikan file statis seperti CSS atau gambar
-app.use(express.static(__dirname));
+ 
+// Menyajikan file statis public
+app.use(express.static('./public'));
 
 // Menyajikan file index.html saat permintaan root dilakukan
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
